2809e9fb325e14228134f673267b0b9e77e29090
Lauri Svan <lauri.svan@sc5.io>
2014-06-22 00:00:33 +0300

    Create missing JSOS directory if it doesn't exist; cleanup

--- index.js
+++ index.js
@@ -5,38 +5,48 @@ var Promise = require('bluebird'),
   path = require('path'),
 	log = require('npmlog'),
 	repl = require('repl'),
-  rc = require('rc');
+  rc = require('rc'),
+	util = require('./lib/util'),
+	nodeUtil = require('util');
 
-// Search for a config from several locations & environment variables; 
-// see https://github.com/dominictarr/rc
-// Then construct a loader with the given configuration
-var config = rc('jsos-init', {
-      logging: {
-        prefix: 'init'
-      },
-      npm: {
-        // Disable spinner, so that it does not screw up our CLI
-        spin: false
-      }
-    }),
-    prefix = config.logging.prefix,
-    loader = Loader(config);
+function loadConfig(fileName) {
+	var defaults = {
+		logging: {
+			prefix: 'init'
+		},
+		npm: {
+			spin: false
+		}
+	};
 
-// Helper functions
-function startPackages(loader) {
+	return new Promise(function(resolve, reject) {
+		// Search for a config from several locations & environment variables;
+		// see https://github.com/dominictarr/rc
+		// Then construct a loader with the given configuration
+		var loadedConfig = rc(fileName, defaults),
+				parsedConfig;
+
+		// Resolve the env. variables nested within the config
+		parsedConfig = util.mapNested(loadedConfig, util.envReplace);
+
+		resolve(parsedConfig);
+	});
+}
+
+function createLauncher(config) {
+	return new Promise(function(resolve, reject) {
+		resolve(Loader(config));
+	});
+}
+
+function startPackages(launcher) {
   return function(packages) {
-    var promises = packages.map(loader.start)
+    var promises = packages.map(launcher.start)
     return Promise.all(promises);
   }
 }
 
 function startREPL(context) {
-  return repl.start({
-      prompt: 'jsos> ',
-      input: process.stdin,
-      output: process.stdout
-    })
-
   // Callback that is invoked when REPL dies
   return new Promise(function(resolve, reject) {
     var cli = repl.start({
@@ -54,20 +64,22 @@ function startREPL(context) {
 
     // Handle the exist function
     cli.on('exit', function() {
-      return resolve();      
+      return resolve();
     })
   });
 }
 
 // Initialize the loader
-var promise = 
-  // Initialize the packet manager
-  loader.init(config)
-  // Then start all the packages found
-  .then(loader.list)
-  // And finally start a REPL, handing the loader as a parameter
-  .then(startPackages(loader))
-  .then(startREPL({ config: config, loader: loader }))
-  .catch(function(err) {
-  	log.error(prefix, 'Initialization failed: %s', err.message);
+var promise = loadConfig('jsos-init')
+	.then(createLauncher)
+	.then(function(launcher) {
+	  // Then start all the packages found, and finally start the REPL
+	  return launcher.init()
+			.then(launcher.list)
+		  .then(startPackages(launcher))
+		  .then(startREPL({ launcher: launcher }))
+	})
+	.catch(function(err) {
+		var message = nodeUtil.format('Initialization failed: %s', err.message);
+  	log.error(message);
   });
